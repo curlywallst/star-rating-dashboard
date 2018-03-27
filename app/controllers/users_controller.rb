242ab1679a2e4307f077a290dbd923ea212ac103
Enoch2k2 <enoch2k2@hotmail.com>
2018-03-27 08:22:14 -0500

    added search feature to search for app user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:find]
+  
+  def search
+
+  end
+  
+  def find
+    if @user
+      flash.discard
+      redirect_to add_user_path(@user.username)
+    else
+      flash[:error] = "User not found"
+      render :search
+    end
+  end
+
   def edit
   end
 
   def update
   end
 
+  private
+    def find_user
+      @user = User.all.find{|user| user.username.downcase == params[:username].downcase}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "application#home"
 
-  get '/users/add-user', to: 'users#edit', as: "add_user"
-  post '/users/add-user', to: 'users#update'
+  get '/users/add-user/:username', to: 'users#edit', as: "add_user"
+  post '/users/add-user/:username', to: 'users#update'
+  get '/users/search-user', to: 'users#search', as: "find_user"
+  post '/users/search-user', to: 'users#find'
 end
