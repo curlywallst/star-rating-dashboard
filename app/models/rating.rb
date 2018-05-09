433d393bd25dff12ea7ae6fb6e70f2290c92e623
curlywallst <nnoyes@comcast.net>
2018-05-09 16:33:27 -0400

    Added rating filter

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,11 +4,15 @@ class Rating < ApplicationRecord
   validates :landing_id, uniqueness: true
 
   def self.star_percent(star)
-    result = where("stars = ?", star).count.to_f * (100.to_f/count.to_f)
+    result = filter_by_star(star).count.to_f * (100.to_f/count.to_f)
     result.round
   end
 
   def self.star_count(star)
-    where("stars = ?", star).count
+    filter_by_star(star).count
+  end
+
+  def self.filter_by_star(star)
+    where("stars = ?", star)
   end
 end
